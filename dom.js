b8ebbda9b8d01d7d77e0b11b3afaf80b6610b636
Brian Takita <brian.takita@gmail.com>
2020-08-05 13:33:25 -0700

    ts outputs commonjs

--- dom.js
+++ dom.js
@@ -1,7 +1,12 @@
-export function _px__rem(rem = 1) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports._rem__px = exports._px__rem = void 0;
+function _px__rem(rem = 1) {
     return rem * parseFloat(getComputedStyle(document.documentElement)
         .fontSize);
 }
-export function _rem__px(px = 16) {
+exports._px__rem = _px__rem;
+function _rem__px(px = 16) {
     return px / _px__rem(1);
 }
+exports._rem__px = _rem__px;
