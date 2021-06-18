778b33711b3183dfc4f05b8f4958ab7282277303
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/index.js
+++
@@ -1,3 +0,0 @@
-export * from './px_rem_';
-export * from './rem_px_';
-//# sourceMappingURL=src/index.js.map
--- src/px_rem_.js
+++
@@ -1,6 +0,0 @@
-import { rem_px_ } from './rem_px_';
-export function px_rem_(px = 16) {
-    return px / rem_px_(1);
-}
-export { px_rem_ as _px_rem, };
-//# sourceMappingURL=src/px_rem_.js.map
